a16f028c254e6e0d5a4587891acfd5e8c99292eb
zarkuslol <lucasferlima8@gmail.com>
2023-08-04 07:13:51 -0300

    Final Authentication Features

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,17 @@
+class RegistrationsController < ApplicationController
+  def create
+    user = User.create!(user_params)
+
+    if user
+      session[:user_id] = user.id
+      render json: { status: :created, user: user }
+    else
+      render json: { status: 500 }
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:email, :password, :password_confirmation)
+    end
+end
