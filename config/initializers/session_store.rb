a7c93e5556e7ee679dde33ae4062dc51618e5f47
zarkuslol <lucasferlima8@gmail.com>
2023-08-04 07:17:24 -0300

    Changing session store configs for production and development

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,6 @@
-Rails.application.config.session_store :cookie_store, key: '_authentication_app', domain: 'http://localhost:3000'
+if Rails.env == "production"
+  Rails.application.config.session_store :cookie_store, key: '_authentication_app'
+else
+  Rails.application.config.session_store :cookie_store, key: '_authentication_app', domain: 'http://localhost:3000'
 
 # Remember to change domain when deploying to production
